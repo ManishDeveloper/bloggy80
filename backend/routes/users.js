8e687e81020646b85cab72046629a3b3d5fcccd9
manish bharti <manishbharti836@gmail.com>
2021-02-27 18:39:29 +0530

    upate api

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,7 +8,7 @@ const Users = require("../models/Users");
 //@route    POST /api/users
 //@desc     Register user
 //@access   Public
-router.post('/',async (req,res)=>{
+router.post('/register',async (req,res)=>{
 
     try {
     let {name,email,password} = req.body;
@@ -19,16 +19,70 @@ router.post('/',async (req,res)=>{
     if(userExist){
         return res.status(400).json({error:'User is already exists!'})
     }
+
+    let salt = await bcrypt.genSalt(10);
+
+    let hasPassword = await bcrypt.hash(password,salt);
    
-    let newUser = new Users({name,email,password});
+    let newUser = new Users({name,email,password:hasPassword});
 
     await newUser.save();
 
-    return res.status(200).json(newUser);
+    const payload = {
+        user:{
+            id:newUser.id
+        }
+    }
+
+    jwt.sign(payload,process.env.JWT_SECRET,{expiresIn:3600000},(err,token)=>{
+        if(err) throw err;
+        return res.status(200).json({token});
+    });
 
     } catch (error) {
-        console.log('error.message');
+        console.log(error.message);
         res.status(500).json({error:'Server Error'})
+    }
+});
+
+
+
+//@route    POST /api/users/login
+//@desc     user login
+//@access   Public
+router.post('/login',async (req,res)=>{
+    try {
+        let {email,password} = req.body;
+
+        //check userExist
+        let userExist = await Users.findOne({email});
+
+        if(!userExist){
+            return res.status(404).json({error:'User is not Exist!'});
+        }
+
+        //check password
+        let checkPass = await bcrypt.compare(password,userExist.password);
+
+        if(!checkPass){
+            return res.status(404).json({error:'Wrong Password!'});
+        }
+
+        let payload = {
+            user:{
+                id:userExist.id
+            }
+        }
+
+        jwt.sign(payload,process.env.JWT_SECRET,{expiresIn:3600000},(err,token)=>{
+            if(err) throw err;
+            return res.status(200).json({token});
+        })
+
+        
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({error:'Server Error'});
     }
 });
 
